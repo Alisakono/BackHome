213f48c2b32f4ea7c9bc2d52410af6394cfb2cb5
Alisakono <alisa.konovalov@web.de>
2023-12-06 16:35:50 +0100

    Д.з от 05.12.23

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] array = new int[] {1, 2, 6, 8, 7, 9, 4, 3};
+        int min = Integer.MAX_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        for (int j : array) {
+            if (j < min) {
+                min = j;
+            } else if (j < min1) {
+                min1 = j;
+            }
+            else if (j<min2){
+                min2 = j;
+            }
+        }
+        System.out.println(min);
+        System.out.println(min1);
+        System.out.println(min2);
+    }
+
+}
