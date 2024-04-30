95bf82b18a1c394685c465fcbff33e43d681fb17
Alisakono <alisa.konovalov@web.de>
2024-04-30 19:22:59 +0200

    test

---
+++ src/main/java/Lesson2/AbstractProduct.java
@@ -0,0 +1,9 @@
+package Lesson2;
+
+import org.example.Product;
+
+public abstract class AbstractProduct  {
+
+    void addToArray(Product[] products){}
+     void removeFromArray(Product[] products){}
+}
